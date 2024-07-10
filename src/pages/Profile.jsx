6a9ea73d22f562512d1a0a0ce4e0cf6ef5f4bb81
shanusingh1199 <shanusingh1199@gmail.com>
2024-07-10 09:35:31 +0530

    create pages and routes

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div>
+      USer profile willl be viewed here
+    </div>
+  );
+}
+
+export default Profile;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div>
+      Do signUp  here
+    </div>
+  );
+}
+
+export default SignUp;
