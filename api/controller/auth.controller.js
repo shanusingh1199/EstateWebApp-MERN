f5e84e1af18dace8054289bdadd6bc7deaa5fe51
shanusingh1199 <shanusingh1199@gmail.com>
2024-07-11 12:45:10 +0530

    complete sigh up page

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-export const signup =async (req,res)=>{
+export const signup =async (req,res,next)=>{
 
     const {username,email,password}=req.body;
     const hashedPassword=bcryptjs.hashSync(password,10);
@@ -9,8 +11,14 @@ export const signup =async (req,res)=>{
     
     try {
         await newUser.save()
-        res.status(200).json("user created successfully")
+        res.status(200).json(new ApiResponse(
+
+            200,
+            newUser,
+            "user created successfully"
+        )
+        )
     } catch (error) {
-        res.status(500).json(error.message)
+        next( new ApiError(500,"error from a function"));
     }
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,84 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 function SignUp() {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({});
+  const navigate=useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(fasle);
+        return;
+      }
+      setError(null)
+    navigate('/sign-in')
+      // console.log(data);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message)
+    }
+  };
   return (
-    <div>
-      Do signUp  here
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4 " onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+          id="username"
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+          id="email"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+          id="password"
+        />
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase
+        hover:opacity-95 disabled:opacity-80 transition transform hover:scale-105 active:scale-95"
+        >
+          {loading? "loading ..." : "Sign Up"}
+        </button>
+      </form>
+      <div className="flex gap-3 mt-5 justify-center">
+        <p>Have an Account</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700 underline">Sign-in</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
