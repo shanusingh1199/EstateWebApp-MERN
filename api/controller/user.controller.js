efa1a7125d7a950c2132206bd09f063976336c33
shanusingh1199 <shanusingh1199@gmail.com>
2024-07-15 14:43:18 +0530

    complete update user functionality

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,31 +1,37 @@
-import User from "../models/user.model.js"
-import { ApiError } from "../utils/ApiError.js"
+import User from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import bcryptjs from 'bcryptjs';
+ 
+export const test = (req, res) => {
+  res.json({
+    message: "hello world",
+  });
+};
 
-export const test = (req,res)=>{
-    res.json({
-        message:"hello world"
-    })
-}
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(new ApiError(401, "you can update your own account!"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
 
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
 
-export const updateUser =async (req,res,next)=>{
-    if(req.user.id !== req.params.id) return next( new ApiError(401,"you can update your own account!"))
-         try {
-            if(req.body.password){
-                req.body.password=bcryptjs.hashSync(req.body.password,10)
-            }
-
-            const updateUser = await User.findByIdAndUpdate(req.params.id,{
-                $set:{
-                    username:req.body.username,
-                    email:req.body.email,
-                    password:req.body.password,
-                    avatar:req.body.avatar,
-                }
-            },{new:true})
-
-            const {password,...rest}=updateUser._doc;
-         } catch (error) {
-            next (error)
-         }
-}
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,15 +2,29 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { useRef } from "react";
 import { useSelector } from "react-redux";
-import {app} from "../firebase.js";
-import {getStorage, getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
-function Profile() {
+import { useDispatch } from "react-redux";
+import { app } from "../firebase.js";
+import {
+  updateUserFailure,
+  updateUsersuccess,
+  updateUsertart,
+} from "../redux/user/userSlice.js";
+import {
+  getStorage,
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+export default function Profile() {
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
-  const[filePerc,setFilePerc]=useState(0);
-  const [fileUploadError,setFileUploadError]=useState(false);
-  const[formData,setFormData]=useState({})
-  const { currentUser } = useSelector((state) => {
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  const dispatch = useDispatch();
+  const { currentUser ,loading,error} = useSelector((state) => {
     return state.user;
   });
   useEffect(() => {
@@ -21,28 +35,58 @@ function Profile() {
 
   const handleFileUpload = (file) => {
     const storage = getStorage(app);
-    const fileName = new Date().getTime + file.name;
+    const fileName = new Date().getTime() + file.name;
     const strorageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(strorageRef, file);
 
-    uploadTask.on("state_changed", (snapshot) => {
-      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      setFilePerc(Math.round(progress));
-    },
-    (error) => {
-      setFileUploadError(true);
-    },
-    ()=>{
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>
-        setFormData(({...formData,avatar:downloadURL}))
-    )
-  }
-)
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUsertart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUsersuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-8">Profile</h1>
-      <form className="flex flex-col gap-5">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
         <input
           onChange={(e) => {
             setFile(e.target.files[0]);
@@ -60,48 +104,57 @@ function Profile() {
           alt="Profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center"
         />
-        <p className='text-sm self-center'>
+        <p className="text-sm self-center">
           {fileUploadError ? (
-            <span className='text-red-700'>
+            <span className="text-red-700">
               Error Image upload (image must be less than 2 mb)
             </span>
           ) : filePerc > 0 && filePerc < 100 ? (
-            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+            <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
           ) : filePerc === 100 ? (
-            <span className='text-green-700'>Image successfully uploaded!</span>
+            <span className="text-green-700">Image successfully uploaded!</span>
           ) : (
-            ''
+            ""
           )}
         </p>
         <input
           type="text"
           id="username"
+          defaultValue={currentUser.username}
           placeholder="Username"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
           id="email"
+          defaultValue={currentUser.email}
           placeholder="email"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
-          type="text"
+          type="password"
           id="password"
           placeholder="password"
           className="border p-3 rounded-lg"
         />
 
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
-          Update
+        <button 
+        disabled={loading}
+        className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          {loading?'Loading':'update'}
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer ">Delete Account</span>
+        
         <span className="text-red-700 cursor-pointer ">Sign Out</span>
+
+        <p className="text-red-700 mt-6">{error?error:""}</p>
+      <p className="text-green-500 mt-6">{updateSuccess ?'User is updated successfully':''}</p>
       </div>
+
     </div>
   );
 }
-
-export default Profile;
